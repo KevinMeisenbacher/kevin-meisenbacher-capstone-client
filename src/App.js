3fc709af58c147cb5fbe9e542b4608a773971612
Kevin Meisenbacher <BeastOfTheEast23@hotmail.ca>
2024-07-01 19:16:45 -0400

    Set up header and homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import './App.scss';
+import Header from './components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <main className="base-container">
-
+      <Header />
+      <HomePage />
     </main>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import logo from '../../img/logo.png';
+
+const Header = () => {
+    return <img src={logo}></img>
+}
+
+export default Header;
