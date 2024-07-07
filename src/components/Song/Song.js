fe67d96ddda4ea40212f0638afe7e3b1b6113553
Kevin Meisenbacher <BeastOfTheEast23@hotmail.ca>
2024-07-07 15:29:29 -0400

    Displayed subgenres for each song's element

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -4,18 +4,23 @@ import './Song.scss'
 
 const Song = ({ song, url }) => {
     const [artist, setArtist] = useState({});
-    const [genre, setGenre] = useState('');
+    const [genre, setGenre] = useState({});
+    const [subgenre, setSubgenre] = useState({});
     
     useEffect(() => {
         axios.get(`${url}/artists/${song.artist_id}`)
             .then(response => setArtist(response.data[0]))
         axios.get(`${url}/genres/${song.genre_id}`)
             .then(response => setGenre(response.data[0]))
+        axios.get(`${url}/subgenres/${artist.subgenre_id}`)
+            .then(response => {
+                if (artist.subgenre_id >= 1) setSubgenre(response.data[0]);
+            })
     }, []);
     return (<div className="song">
         <p>{song.song_name}</p>
         <p>{artist.artist_name}</p>
-        <p>{genre.genre_name}</p>
+        <p>{subgenre.id >= 1 ? subgenre.subgenre_name : genre.genre_name}</p>
     </div>)
 }
 
